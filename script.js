eb5a83529f155e1d539a4eb3473d11b7ef9f2fe8
erenmikasayeager2005-ops <erenmikasayeager2005@gmail.com>
2026-02-01 14:29:41 +0530

    Implement button movement on hover and touch
    Add functionality to move button on mouseover or touchstart

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const noBtn = document.getElementById("no");
+const text = document.getElementById("runText");
 
+noBtn.addEventListener("mouseover", run);
+noBtn.addEventListener("touchstart", run);
+
+function run() {
+  text.style.display = "block";
+  const x = Math.random() * (window.innerWidth - 100);
+  const y = Math.random() * (window.innerHeight - 100);
+  noBtn.style.left = x + "px";
+  noBtn.style.top = y + "px";
+}
